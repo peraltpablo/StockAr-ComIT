7cc5224af4dc08631cfaadfc9687a4c9d013c90d
unknown <peraltapablo@live.com>
2021-06-21 11:45:51 -0300

    panel funcionando, login ok, pagina alta prod en proceso

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -4,22 +4,21 @@ var $$ = Dom7;
 var app = new Framework7({
     // App root element
     root: '#app',
-    // App Name
+    // NOMBRE DE LA APP
     name: 'My App',
-    // App id
+    // ID DE LA APP
     id: 'com.myapp.test',
-    // Enable swipe panel
+    // SWIPE PANELES
     panel: {
       swipe: 'left',
     },
-    // Add default routes
+    // RUTAS
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
+        {path: '/index/', url: 'index.html',},
+        {path: '/inicio/',url: 'inicio.html',},
+        {path: '/ingreso/',url: 'ingreso.html',},
+        {path: '/registro/',url: 'registro.html',},
     ]
-    // ... other parameters
   });
 
 //CREO LA VISTA DE LA APP
@@ -30,24 +29,144 @@ $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
-// INICIALIZACIÓN DE PÁGINA INDEX
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
-    $$("#ingresar").on('click', fnIngreso);
+
 });
 
-// AUTENTICACIÓN
+// INICIALIZACIÓN DE PÁGINA USUARIO INGRESO
+$$(document).on('page:init', '.page[data-name="ingreso"]', function (e) {
+    $$("#ingresar").on('click', fnIngreso);
+    $$("#registracion").on('click', fnRegistracion);
+    $$("#cerrarSesion").on('click', fnCerrarSesion);
+    observador();
+});
+
+// INICIALIZACIÓN DE PÁGINA USUARIO REGISTRO
+$$(document).on('page:init', '.page[data-name="registro"]', function (e) {
+    $$("#registrar").on('click', fnRegistro);
+});
+
+// AUTENTICACIÓN USUARIO EXISTENTE
 function fnIngreso() {
     var email = $$("#email").val();
     var contrasena = $$("#contrasena").val();
     console.log(email);
     console.log(contrasena);
 
-    firebase.auth().createUserWithEmailAndPassword(email,contrasena)
+    firebase.auth().signInWithEmailAndPassword(email, contrasena)
     .then(function(){
-        console.log("Registrado!");
+        console.log("Ingreso!");
+        mainView.router.navigate('/inicio/');
+
     })
     .catch(function(error){
+        if (error.code == "auth/invalid-email") {
+            alert("Usuario Incorrecto");
+        }
     console.error(error.code);
     console.error(error.message);
     });
 };
+
+//VA A PÁGINA DE REGISTRO
+function fnRegistracion() {
+    mainView.router.navigate('/registro/')
+}
+
+// AUTENTICACIÓN NUEVO USUARIO
+function fnRegistro() {
+    var email_nuevo = $$("#email_nuevo").val();
+    var contrasena_nuevo = $$("#contrasena_nuevo").val();
+    console.log(email_nuevo);
+    console.log(contrasena_nuevo);
+
+    firebase.auth().createUserWithEmailAndPassword(email_nuevo,contrasena_nuevo)
+    .then((userCredential) => {
+        $$("#registro_ok").html("Registro exitoso! <a href='/ingreso/'>Iniciá sesión</a>")
+        //mainView.router.navigate('/ingreso/')
+    // Signed in
+    //var user = userCredential.user;
+    // ...
+    })
+    .catch((error) => {
+        if (contrasena.length < 6) {
+            alert("La contraseña debe tener al menos 6 caracteres")
+        }
+    console.error(error.code);
+    console.error(error.message);
+    });
+};
+
+//FUNCION OBSERVADOR PARA SABER SI EXISTE USUARIO ACTIVO
+
+function observador() {
+    firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log("existe");
+        fnAparece();
+    } else {
+        console.log("no existe")
+        fnDesaparece();
+  }
+});
+};
+
+observador();
+
+function fnAparece() {
+    $$("#aparece_cerrar").removeClass('oculto').addClass('visible');
+};
+
+function fnDesaparece() {
+    $$("#aparece_cerrar").removeClass('visible').addClass('oculto');
+};
+
+function fnCerrarSesion() {
+    firebase.auth().signOut()
+    .then(function(){
+        console.log("saliendo");
+
+    })
+    .catch(function(){
+        console.error(error.code);
+        console.error(error.message);
+    })
+}
+
+//BASE DE DATOS
+var db = firebase.firestore();
+    colProductos = db.collection("productos");
+
+    /*var data = {nombre: "Pepe", rol: "developer"};
+    MiID = "pepe@hotmail.com"
+    colPersonas.doc(MiID).set(data)
+    .then(function(miVarDeDocRef) {
+        console.log("OK! Con el ID: " + miVarDeDocRef.id)
+    })
+    .catch(function(datosDelError) {
+        console.error("Error: " + datosDelError);
+    });*/
+
+    /*var data = {nombreProd: "", marca: "", detalle: "", alerta: "", alertaMin:""};
+    
+    MiID = "codigo del producto"
+    colProductos.doc(MiID).set(data)
+    .then(function(miVarDeDocRef) {
+        console.log("OK! Con el ID: " + miVarDeDocRef.id)
+    })
+    .catch(function(datosDelError) {
+        console.error("Error: " + datosDelError);
+    });
+
+    /*colPersonas.get()
+    .then(function(q5) {
+        q5.forEach(function(doc) {
+            nombre = doc.data().nombre;
+            rol = doc.data().rol;
+
+            $$("#datosPersonas").append('<p>'+nombre+'</p><p>'+rol+'</p>');
+        })
+    })
+    .catch(function(err) {
+        console.log("Error" + err);
+    })*/
